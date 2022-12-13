b894b53432f32e5c5dc03f0612c01630c32fcca7
Luis Vegas <luisvegasmor@gmail.com>
2022-12-12 22:07:54 -0400

    feat: REPL (early WIP)

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,38 @@
 object Main {
+  import scala.io.Source.fromFile
   import fastparse._
+  import scala.io.StdIn.readLine
+
   sealed trait Operator
+
   case object Plus extends Operator
+
   case object Div extends Operator
+
   case object Minus extends Operator
 
   def main(args: Array[String]): Unit = {
-    val p = parse("false", new Parser().bool(_))
-    println(p)
+    args match {
+      case Array(file) => ???
+      case _ => run()
+    }
+  }
+
+  def runFromFile(file: String) = {
+    ???
+  }
+
+  def run() : Unit = {
+    var continue: Boolean = true
+    var tokens: Array[String] = Array()
+    while (continue) {
+      print("> ")
+      var line = readLine()
+      tokens = line.split("\\s+")
+
+      if (line == ":q") continue = false
+      val p = parse(line, new Parser().factor(_))
+      println(p)
+    }
   }
 }
