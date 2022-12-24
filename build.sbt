88c6997a1bca6e6f69c3f2599cee94d61286fae1
Luis Vegas <luisvegasmor@gmail.com>
2022-12-24 17:31:22 -0400

    feat: added tests

--- build.sbt
+++ build.sbt
@@ -2,7 +2,9 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "2.13.8"
 
-libraryDependencies +="com.lihaoyi" %% "fastparse" % "2.2.2"
+libraryDependencies += "com.lihaoyi" %% "fastparse" % "2.2.2"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.11" % "test"
+
 lazy val root = (project in file("."))
   .settings(
     name := "Nomi"
---
+++ src/test/scala/NomiTest.scala
@@ -0,0 +1,29 @@
+import org.scalatest.funsuite.AnyFunSuite
+import fastparse._
+
+class NomiTest extends AnyFunSuite {
+  val Parsed.Success(value, _) = parse("123", new Parser().number(_))
+  test("1 should be number") {
+    assert(value === Number(123))
+  }
+
+  val Parsed.Success(t, _) = parse("true", new Parser().bool(_))
+  test("true should be boolean") {
+    assert(t === Bool(true))
+  }
+
+  val Parsed.Success(f, _) = parse("false", new Parser().bool(_))
+  test("false should be boolean") {
+    assert(f === Bool(false))
+  }
+
+  val Parsed.Success(b, _) = parse("12 + 32", new Parser().factor(_))
+  test("expression is binary") {
+    assert(b === Binary(Number(12), "+", Number(32)))
+  }
+
+  val Parsed.Success(a, _) = parse("let foo=32", new Parser().assignment(_))
+  test("Expr is assignment to 32") {
+    assert(a === Assign("foo", Number(32)))
+  }
+}
