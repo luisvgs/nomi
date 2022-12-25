d326c3848c156d86fe1fe2ae19a7ce84a62c6409
Luis Vegas <luisvegasmor@gmail.com>
2022-12-25 16:35:32 -0400

    feat: extended Operators and Func expressions

--- src/main/scala/Expr.scala
+++ src/main/scala/Expr.scala
@@ -1,6 +1,7 @@
 sealed trait Expr
 case class Assign(name: String, value: Expr) extends Expr
 case class Str(string: String) extends Expr
+case class Func(name: String, args: String, stmt: Seq[Expr]) extends Expr
 case class Variable(name: String) extends Expr
 case class Number(n: Int) extends Expr
 case class Bool(b: Boolean) extends Expr
@@ -9,5 +10,9 @@ case class Identifier(name: String) extends Expr
 
 sealed trait Operator
 case object Plus extends Operator
-case object Div extends Operator
 case object Minus extends Operator
+case object Or extends Operator
+case object And extends Operator
+case object LessThan extends Operator
+case object GreaterThan extends Operator
+case object Equal extends Operator
